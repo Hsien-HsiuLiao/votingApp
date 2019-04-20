5a4f53561d8f061599a885b6885611a02b5e658b
Hsien <hsien-hsiu.liao@yandex.com>
2019-04-19 19:01:10 -0700

    update contract

--- contracts/vote.sol
+++ contracts/vote.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.5.0;
 
 contract vote {
 
+    struct Candidate [
+        uint id;
+        string name;
+        uint voteCount;
+    }
     constructor() public {
         owner = msg.sender;
     }
